72fca4589606413d01744d575b399459dcd84065
Mateus Felipe do Nascimento <maateus.n@gmail.com>
2021-02-25 12:11:50 -0300

    separate repository

--- src/database/migrations/1614209884517-CreateUsers.ts
+++ src/database/migrations/1614209884517-CreateUsers.ts
@@ -1,7 +1,6 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateUsers1614209884517 implements MigrationInterface {
-
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
