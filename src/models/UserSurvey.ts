9762fb72664fdbf823e48488f799dafb65840c15
Mateus Felipe do Nascimento <maateus.n@gmail.com>
2021-02-27 09:58:48 -0300

    relations between tables

--- src/models/UserSurvey.ts
+++ src/models/UserSurvey.ts
@@ -1,5 +1,7 @@
-import {Column, CreateDateColumn, Entity, PrimaryColumn} from 'typeorm';
+import {Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn} from 'typeorm';
 import { v4 as uuid } from "uuid"; 
+import { User } from './User';
+import { Survey } from './Survey'
 
 @Entity("users_survey")
 class UserSurvey {
@@ -9,8 +11,16 @@ class UserSurvey {
     @Column()
     user_id: string;
 
+    @ManyToOne(() => User)
+    @JoinColumn({ name: "user_id"})
+    user: User;
+
     @Column()
     survey_id: string;
+
+    @ManyToOne(()=> Survey)
+    @JoinColumn({ name: "survey_id"})
+    survey: Survey;
 
     @Column()
     value: number;
